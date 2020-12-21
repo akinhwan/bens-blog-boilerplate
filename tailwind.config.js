8c591f82e2accbc25125d878c8ca28291fb064e1
Andrew Kim <akinhwan@gmail.com>
2020-12-20 21:22:08 -0500

    empty purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,17 @@ module.exports = {
   //   enabled: process.env.NODE_ENV === 'production',
   //   content: ['public/index.html', 'src/**/*.js', 'src/**/*.vue']
   // },
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  },
+  purge: [],
+  // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  // enabled: process.env.NODE_ENV === 'production',
+  // content: [
+  //   'components/**/*.vue',
+  //   'layouts/**/*.vue',
+  //   'pages/**/*.vue',
+  //   'plugins/**/*.js',
+  //   'nuxt.config.js'
+  // ]
+  // },
   theme: {
     extend: {
       colors: {
